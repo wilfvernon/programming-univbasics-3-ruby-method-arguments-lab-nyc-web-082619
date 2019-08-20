a155e4a5cb984b8772296ccf8926a907353ca7fe
Wilfred Alec Vernon <wilf.vernon@gmail.com>
2019-08-20 14:09:37 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,6 +7,10 @@
  end
  
  def introduction_with_language(name, language)
+   p "Hi, my name is #{name} and I am learning to program in #{language}"
+ end
+ 
+ def introduction_with_language_optional(name, language = "Ruby")
    p 
 
 
